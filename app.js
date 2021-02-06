10d860a27ba8bbfb305a6c866e76360c031acd9f
ggnight0802 <ggnight0802@gmail.com>
2021-02-06 20:10:26 +0900

    Applying res.locals as global Variable

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import {localsMiddleware} from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -10,16 +11,17 @@ import routes from "./routes";
 
 const app = express()
 
+app.use(helmet());                                      //  helmet -> security를 담당하는 미들웨어
 app.set("view engine","pug");
 app.use(cookieParser());                                //  cookieParser -> cookie를 전달받아서 사용할 수 있도록 만들어주는 미들웨어 (ex 사용자 인증)
 app.use(bodyParser.json());                             //  bodyParser -> 사용자가 웹사이트로 전달하는 정보들을 검사하는 미들웨어 (requset 정보에서 form,JSON 형태로 이루어진 body를 검사)
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(helmet());                                      //  helmet -> security를 담당하는 미들웨어
 app.use(morgan("dev"));                                 //  morgan -> application의 log를 담당하는 미들웨어
+app.use(localsMiddleware);                              //  A middleware with no mount path -> excuted for every request from app
 
 app.use(routes.home,globalRouter);
 app.use(routes.users,userRouter);
 app.use(routes.videos,videoRouter);
 
-
+ 
 export default app;
---
+++ middlewares.js
@@ -0,0 +1,19 @@
+/*
+    res.locals
+
+    res.locals의 프로퍼티들은 request의 라이프 타임 동안에만 유효하다.
+    html/view 클라이언트 사이드로 변수들을 보낼 수 있으며, 그 변수들은 오로지 거기서만 사용할 수 있다.
+
+    locals에 로컬 변수를 저장하면 이 변수를 view안에 있는 템플릿(.pug파일)들에서 사용 할 수 있다 => 템플릿을 직접 수정하지 않고, locals 내부의 변수들만 수정하면 된다.
+    ==>이 미들웨어는 코드 사이에 들어가 있기 때문에, next() 를 호출해야한다. 다음 함수들로 넘어가야하기 때문이다.
+
+*/
+
+import routes from "./routes";
+
+export const localsMiddleware = (req, res, next) => {
+
+    res.locals.siteName = "WeTube";
+    res.locals.routes = routes;
+    next();
+};
