168d0ee109e76362f0384dab15076dd560377126
ggnight0802 <ggnight0802@gmail.com>
2021-02-07 17:56:23 +0900

    Applying 'form tag' on 'header.pug'

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,16 +1,14 @@
 
-export const videoHome = (req, res) => 
-    res.render("home",{pageTitle: "Home"});
-export const videoSearch = (req, res) => 
-    res.render("search",{pageTitle: "Search"});
+export const videoHome = (req, res) => res.render("home",{pageTitle: "Home"});
+export const videoSearch = (req, res) => {
+    const {
+        query: {term: searchingBy}
+    } = req;
+    res.render("search",{pageTitle: "Search", searchingBy: searchingBy})
+};
 
-export const videos = (req, res) => 
-    res.render("videos",{pageTitle: "Videos"});
-export const upload = (req, res) => 
-    res.render("upload",{pageTitle: "Upload"});
-export const videoDetail = (req, res) => 
-    res.render("videoDetail",{pageTitle: "Video Detail"});
-export const editVideo = (req, res) => 
-    res.render("editVideo",{pageTitle: "Edit video"});
-export const deleteVideo = (req, res) => 
-    res.render("deleteVideo",{pageTitle: "Delete Video"});
+export const videos = (req, res) => res.render("videos",{pageTitle: "Videos"});
+export const upload = (req, res) => res.render("upload",{pageTitle: "Upload"});
+export const videoDetail = (req, res) => res.render("videoDetail",{pageTitle: "Video Detail"});
+export const editVideo = (req, res) => res.render("editVideo",{pageTitle: "Edit video"});
+export const deleteVideo = (req, res) => res.render("deleteVideo",{pageTitle: "Delete Video"});
