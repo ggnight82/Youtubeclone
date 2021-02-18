c861f89f827d598b73048b3eef5067385f6371ad
ggnight0802 <ggnight0802@gmail.com>
2021-02-18 21:09:47 +0900

    postEditVideo&getEditVideo

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import routes from "../routes";
-import { deleteVideo, editVideo, getUpload,   postUpload, videoDetail, videos } from "../controller/videoController";
+import { deleteVideo, getEditVideo, getUpload,   postEditVideo,   postUpload, videoDetail, videos } from "../controller/videoController";
 import { uploadVideo } from "../middlewares";
 
 
@@ -8,12 +8,20 @@ const videoRouter = express.Router();
 
 videoRouter.get(routes.videos, videos);
 
+// Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo , postUpload);
 
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
+
+// Video Edit
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Video Delete
+videoRouter.get(routes.deleteVideo, deleteVideo);
+
 
 
 export default videoRouter;
