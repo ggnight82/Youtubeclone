9f2eb18a70285d50777f511e16953c4694d2370e
ggnight0802 <ggnight0802@gmail.com>
2021-02-24 22:36:24 +0900

    applying ESLint

--- controller/videoController.js
+++ controller/videoController.js
@@ -33,7 +33,6 @@ export const postUpload = async (req, res) => {
          title,
          description
      });
-     console.log(newVideo);
      res.redirect(routes.videoDetail(newVideo.id));
           
 };
